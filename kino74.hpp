a1ff7d323d7cfc1d2d77406205bc00ef6e63be88
captnjohnny1618 <jhoffm@vt.edu>
2016-08-22 15:56:28 -0700

    reading in program now

--- kino74.hpp
+++ kino74.hpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
 
 class kino74{
@@ -12,7 +13,6 @@ class kino74{
 
     // Counters:
     unsigned char PC;
-    unsigned char RING;
 
     // Memory:
     unsigned char RAM[16];
@@ -21,6 +21,8 @@ public:
     void initialize();
     void exec_cycle();
     void print_state();
+    void load_program(std::string);
+    void dump_ram();    
 };
 
 void kino74::initialize(){
@@ -29,9 +31,30 @@ void kino74::initialize(){
         RAM[i]=0;
     }
     
-    // Reset Program and Ring Counter
+    // Reset all registers
     PC=0;
-    RING=0;
+    ACC=0;
+    B=0;
+    MAR=0;
+    IR=0;
+    OUT=0;
+}
+
+void kino74::load_program(std::string filepath){
+    std::ifstream file (filepath.c_str(),std::ios::binary);    
+    if (file){
+        file.read((char*)RAM,16L);
+    }
+    else{
+        std::cerr << "Program file not found." << std::endl;
+        exit(1);
+    }
+}
+
+void kino74::dump_ram(){
+    for (int i=0;i<16;i++){
+        std::cout << (int)RAM[i] << std::endl;
+    }
 }
 
 void kino74::exec_cycle(){
@@ -40,7 +63,7 @@ void kino74::exec_cycle(){
     MAR=PC;
 
     // (2) Increment state (Increment the PC)
-    PC+=1;
+    PC++;
 
     // (3) Memory State (load IR with RAM[MAR])
     IR=RAM[MAR];
@@ -66,7 +89,7 @@ void kino74::exec_cycle(){
         break;
 
     default:
-        std::cout << "Op code not found" << std::endl;
+        std::cerr << "Op code not found" << std::endl;
         exit(1);            
     }
 }
@@ -75,29 +98,12 @@ using namespace std;
 
 void kino74::print_state(){
 
-    // Registers:
-    unsigned char ACC;
-    unsigned char B;
-    unsigned char MAR;
-    unsigned char IR;
-    unsigned char OUT;
-
-    // Counters:
-    unsigned char PC;
-    unsigned char RING;
-
-    // Memory:
-    unsigned char RAM[16];
-
-    std::cout << "PC: " << (unsigned int)IR << std::endl;
+    std::cout << "PC: " << (unsigned int)PC << std::endl;
     std::cout << "MAR: " << (unsigned int)MAR << std::endl;
     std::cout << "IR: " << (unsigned int)IR << std::endl;
-    std::cout << std::endl;
     std::cout << "Accumulator: " << (unsigned int)ACC << std::endl;
     std::cout << "B: " <<  (unsigned int) B << std::endl;
-
-
-
-    
-
+    std::cout << "Out: " <<  (unsigned int) OUT << std::endl;
+    std::cout << "--------------------------------" << std::endl;
+        
 }
--- main.cpp
+++ main.cpp
@@ -4,12 +4,20 @@ int main(int argc, char ** argv){
 
     kino74 myKino74;
 
-    myKino74.initialize();
+    std::string prog_file(argv[1]);
 
-    while (1){
+    myKino74.initialize();
+    
+    myKino74.load_program(prog_file);
+    
+    myKino74.dump_ram();
+
+    int pc=0;
+    while (pc<16){
 
         myKino74.exec_cycle();
         myKino74.print_state();
+        pc++;
     }
     
 }
